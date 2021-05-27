b3264fce14adebfc6fed877a018f7279d053b242
dillonmemo <arta1069@gmail.com>
2021-05-27 10:47:29 +0900

    refactor error exception

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -10,20 +10,28 @@ export class WeatherService {
 
   @Interval(5000)
   async devInterval(): Promise<void> {
-    const message = '5초단위로 실행(테스트)';
-    const args = {
-      body: message,
-    };
-    await got.post(
-      'https://wh.jandi.com/connect-api/webhook/20585156/cc73f65de82e1e591a8b2166c7b91d28',
-      {
-        headers: {
-          Accept: 'application/vnd.tosslab.jandi-v2+json',
-          'Content-type': 'application/json',
+    try {
+      const message = '5초단위로 실행(테스트)';
+      const args = {
+        body: message,
+      };
+      const response = await got.post(
+        'https://wh.jandi.com/connect-api/webhook/20585156/cc73f65de82e1e591a8b2166c7b91d28',
+        {
+          headers: {
+            Accept: 'application/vnd.tosslab.jandi-v2+json',
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(args),
         },
-        body: JSON.stringify(args),
-      },
-    );
+      );
+
+      if (response.statusCode !== 200) {
+        throw new Error(response.statusMessage);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 // ?lat=37.5170&lon=127.0289&appid= 926458919a73df77645df1c77ef9fa5c&units=metric
