212ba1b04c75cae9766f49d0b622f34b8e99a494
dillonmemo <arta1069@gmail.com>
2021-05-30 17:17:28 +0900

    update db connect module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,12 @@ import { SearchModule } from './search/search.module';
 import { MenuModule } from './menu/menu.module';
 import { Menu } from './menu/entities/menu.entity';
 
-console.log('NODE_ENV', process.env.NODE_ENV);
+console.log(
+  'NODE_ENV',
+  process.env.NODE_ENV,
+  'DATABASE_URL',
+  process.env.DATABASE_URL,
+);
 
 @Module({
   imports: [
@@ -18,22 +23,26 @@ console.log('NODE_ENV', process.env.NODE_ENV);
       ignoreEnvFile: process.env.NODE_ENV === 'prod', // 서버에 deploy 할 때 환경변수 파일을 사용하지 않습니다.
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
       }),
     }),
     /** @description - https://docs.nestjs.com/techniques/database#typeorm-integration */
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: process.env.NODE_ENV !== 'prod',
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
+      synchronize: true, // process.env.NODE_ENV !== 'prod',
       logging: true, // process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
       entities: [Menu],
     }),
